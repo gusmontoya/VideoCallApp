43a975512e2b590f50f674ef79c32d14ff2deae6
gusmontoya <gus.montoya@gmail.com>
2022-03-19 16:07:27 -0700

    created search

--- src/screens/ContactsScreen/index.js
+++ src/screens/ContactsScreen/index.js
@@ -1,40 +1,51 @@
-import React from 'react';
-import { View, FlatList, Text, StyleSheet, TextInput} from 'react-native';
-import contacts from '../../../assets/data/contacts.json';
+import React, {useState, useEffect} from 'react';
+import {View, FlatList, Text, StyleSheet, TextInput} from 'react-native';
+import dummyContacts from '../../../assets/data/contacts.json';
 
 const ContactsScreen = () => {
+    const [searchTerm, setSearchTerm] = useState('');
+    const [filteredContacts, setFilteredContacts] = useState(dummyContacts);
 
-    return(
+    
+
+    console.log(searchTerm);
+    return (
         <View style={styles.page}>
-            <TextInput style={styles.searchInput} placeholder="Search..." />
-        <FlatList 
-          data={contacts} 
-          renderItem={({item}) => (<Text style={styles.contactName}>{item.user_display_name}</Text>
-          )}
-          ItemSeparatorComponent={() => <View style={styles.separator} />}
-        />
-      </View>
-    )
-}
+            <TextInput
+                value={searchTerm}
+                onChangeText={setSearchTerm}
+                style={styles.searchInput}
+                placeholder="Search..."
+            />
+            <FlatList
+                data={filteredContacts}
+                renderItem={({item}) => (
+                <Text style={styles.contactName}>{item.user_display_name}</Text>
+                )}
+                ItemSeparatorComponent={() => <View style={styles.separator} />}
+            />
+        </View>
+    );
+};
 
 const styles = StyleSheet.create({
-    page: {
-      padding: 15,
-    },
-    contactName: {
-      fontSize: 16,
-      marginVertical: 10,
-    },
-    separator: {
-      width: '100%',
-      height: 1,
-      backgroundColor: '#f0f0f0',
-    },
-    searchInput: {
-        backgroundColor: '#f0f0f0',
-        padding: 10,
-        borderRadius: 5,
-    },
-    });
+  page: {
+    padding: 15,
+  },
+  contactName: {
+    fontSize: 16,
+    marginVertical: 10,
+  },
+  separator: {
+    width: '100%',
+    height: 1,
+    backgroundColor: '#f0f0f0',
+  },
+  searchInput: {
+    backgroundColor: '#f0f0f0',
+    padding: 10,
+    borderRadius: 5,
+  },
+});
 
 export default ContactsScreen;
