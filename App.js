104c6f2e1f8304b6dd944d4f89192406e7da2234
gusmontoya <gus.montoya@gmail.com>
2022-03-18 22:24:47 -0700

    deleting unneeded components in App.js

--- App.js
+++ App.js
@@ -26,16 +26,11 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+const App => {
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+    <SafeAreaView>
+      <StatusBar barStyle={'dark-content'} />
       
     </SafeAreaView>
   );
